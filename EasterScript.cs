188ea0042243494eec139d38fefd6d78aac066e3
Nova_Max <tom.maier@abwesend.de>
2019-05-23 23:50:37 +0200

    V1.0.0 Initial Commit
    V1.0.0 Initial Commit

---
+++ EasterScript.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UMod;
+using Synth.mods.utils;
+using Synth.mods.events;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class EasterScript : ModScript, ISynthRidersEvents
+{
+    public GameObject egg = null;
+
+    public override void OnModLoaded()
+    {
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+    }
+
+    public override void OnModUnload()
+    {
+        SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+    }
+
+    public void OnRoomLoaded()
+    {
+
+    }
+
+    public void OnRoomUnloaded()
+    {
+
+    }
+
+    public override void OnModUpdate()
+    {
+        
+    }
+
+    private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        if (arg0.name.Contains("Stage"))
+        {
+            egg = ModAssets.Instantiate<GameObject>("Easter_pre");
+        }
+        else{
+            EggScript.DestroyEggScript();
+            UnityEngine.Object.Destroy(egg);
+            egg = null;
+        }
+    }
+
+    public void OnGameStageLoaded(TrackData trackData)
+    {
+        
+    }
+
+    public void OnGameStageUnloaded()
+    {
+        EggScript.DestroyEggScript();
+        UnityEngine.Object.Destroy(egg);
+        egg = null;
+    }
+
+    public void OnScoreStageLoaded()
+    {
+
+    }
+
+    public void OnScoreStageUnloaded()
+    {
+
+    }
+
+    public void OnNoteFail(PointsData pointsData)
+    {
+
+    }
+
+    public void OnPointScored(PointsData pointsData)
+    {
+
+    }
+
+    public void OnSongFinished(SongFinishedData songFinishedData)
+    {
+
+    }
+
+
+    public void OnSongFailed(TrackData trackData)
+    {
+
+    }
+}
---
+++ EggScript.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EggScript : MonoBehaviour
+{
+    public static EggScript s_instance;
+
+    public Mesh egg_mesh = null;
+    public Material egg_material = null;
+
+    void Awake()
+    {
+        if (s_instance != null)
+        {
+            Destroy(this);
+        }
+
+        s_instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    void Start()
+    {
+        if (s_instance == null) { return; }
+
+        egg_mesh = s_instance.gameObject.GetComponent<MeshFilter>().sharedMesh;
+        egg_material = s_instance.gameObject.GetComponent<MeshRenderer>().material;
+        MonoBehaviour behaviour = (MonoBehaviour)UnityEngine.Object.FindObjectOfType(typeof(MonoBehaviour));
+        behaviour.StartCoroutine(Change(egg_mesh, egg_material));
+        s_instance.gameObject.GetComponent<MeshFilter>().sharedMesh = null;
+    }
+
+    void Update()
+    {
+       
+    }
+
+    IEnumerator Change(Mesh mesh, Material material)
+    {
+        yield return new WaitForSeconds(1f);
+
+
+        GameObject notesParent = GameObject.Find("[SongTrack]");
+
+        int count = notesParent.transform.childCount;
+
+        for (int i = 0; i < count; i++)
+        {
+            Transform note = notesParent.transform.GetChild(i);
+
+            MeshFilter[] mfs = note.GetComponentsInChildren<MeshFilter>(true);
+            MeshRenderer[] mrs = note.GetComponentsInChildren<MeshRenderer>(true);
+
+            mfs[0].sharedMesh = mesh;
+            mrs[0].material.SetTexture("_MainTex", material.GetTexture("_MainTex"));
+        }
+    }
+
+    public static void DestroyEggScript()
+    {
+        if (s_instance == null) { return; }
+
+        Destroy(s_instance);
+    }
+}
